06e580b0f2c3dea3be0a6157f8ea5e46e67e6fab
DesKay <102972676+DesKay@users.noreply.github.com>
2022-08-08 02:46:18 -0800

    Add files via upload
    finding the average of even numbers less than a number

--- main.cpp
+++ main.cpp
@@ -1,32 +1,18 @@
-#include <bits/stdc++.h>
+#include <iostream>
+
 using namespace std;
-
-int sumOfPrimes(int n)
-{
-    bool prime[n + 1];
-
-    memset(prime, true, n + 1);
-
-    for (int p = 2; p * p <= n; p++) {
-
-       if (prime[p] == true) {
-
-            for (int i = p * 2; i <= n; i += p)
-                prime[i] = false;
-        }
-    }
-
-    int sum = 0;
-    for (int i = 2; i <= n; i++)
-        if (prime[i])
-            sum += i;
-    return sum;
-}
 
 int main()
 {
-    int n = 100;
-    //where n is the last number on the range
-    cout <<sumOfPrimes(n);
+    int number = 2;
+    int sum = 0;
+    int average;
+    while(number < 1000)
+    {
+        sum += number;
+        number += 2;
+    }
+    average = sum / (1000 / 2);
+    cout << average << endl;
     return 0;
 }
